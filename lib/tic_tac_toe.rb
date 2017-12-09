d5fe0f709b44cc95accb5a2ac55e20fafe8aac50
William Kann <william.l.kann@gmail.com>
2017-12-09 07:20:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,6 +35,18 @@ WIN_COMBINATIONS = [
     @board.count{|token| token == "X" || token == "O"}
   end
 
+  def turn(@board)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(board, index) == true
+      move(board, index, "#{current_player(board)}")
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
+
 end
 
 
