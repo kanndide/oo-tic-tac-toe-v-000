3a03fb7ac113f532fe805be7a779686276f2d11d
William Kann <william.l.kann@gmail.com>
2017-12-09 08:03:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,14 +65,7 @@ WIN_COMBINATIONS = [
 
   def won?
     WIN_COMBINATIONS.each do |win_combination|
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
-      position_1 = @board[win_index_1] # load the value of the board at win_index_1
-      position_2 = @board[win_index_2] # load the value of the board at win_index_2
-      position_3 = @board[win_index_3] # load the value of the board at win_index_3
-
-      if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+      if @board[win_combination[0]] == "X" && @board[win_combination[1]] == "X" && @board[win_combination[2]] == "X" || @board[win_combination[0]] == "O" && @board[win_combination[1]] == "O" && @board[win_combination[2]] == "O"
         return win_combination # return the win_combination indexes that won.
       end
     end
