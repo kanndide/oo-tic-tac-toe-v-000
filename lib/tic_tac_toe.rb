6bcb35a24d51a7e8a7bf2b88f5e1280af2c6bb5c
William Kann <william.l.kann@gmail.com>
2017-12-09 08:04:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,10 +66,14 @@ WIN_COMBINATIONS = [
   def won?
     WIN_COMBINATIONS.each do |win_combination|
       if @board[win_combination[0]] == "X" && @board[win_combination[1]] == "X" && @board[win_combination[2]] == "X" || @board[win_combination[0]] == "O" && @board[win_combination[1]] == "O" && @board[win_combination[2]] == "O"
-        return win_combination # return the win_combination indexes that won.
+        return win_combination 
       end
     end
     return false
+  end
+
+  def full?
+    @board.include?(" ") == false
   end
 
 end
